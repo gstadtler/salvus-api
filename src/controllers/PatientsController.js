c0e684b404337eeb809fcd64467b80bb30421779
Gabriel Stadtler <ghstadtler@gmail.com>
2020-09-03 04:54:59 -0300

    criando as rotas e o controller de patients

---
+++ src/controllers/PatientsController.js
@@ -0,0 +1,82 @@
+const connection = require('../database/connection');
+
+module.exports = {
+	async index(request, response) {
+		try {
+			const patients = await connection('patients');
+
+			return response.json(patients);
+		} catch(err) {
+			console.log(err);
+		}
+		
+	},
+
+	async show(request, response) {
+		try {
+			const patient = await connection('patients');
+
+			return response.json(patient);
+		} catch(err) {
+			console.log(err);
+		}
+		
+	},
+
+	async create(request, response) {
+		try {
+			const { cpf, name, email, phone_number, 
+							street, house_number, city, uf } = request.body;
+
+			await connection('patients').insert({
+				cpf,
+				name,
+				email,
+				phone_number,
+				street,
+				house_number,
+				city,
+				uf,
+			});
+
+			return response.status(201).send();
+		} catch(err) {
+			console.log(err)
+		}
+		
+	},
+
+	async update(request, response) {
+		try {
+			const { cpf, name, email, phone_number, 
+				street, house_number, city, uf } = request.body;
+			const { id } = request.params;
+
+			await connection('patients').update({ 
+				cpf, 
+				name, 
+				email, 
+				phone_number, 
+				street, 
+				house_number, 
+				city, 
+				uf }).where({ id });
+
+			return response.send();
+		} catch(err) {
+				console.log(err)
+		}
+	},
+
+	async delete(request, response) {
+		try {
+			const { id } = request.params;
+
+			await connection('patients').where({ id }).delete();
+
+			return response.status(204).send();
+		} catch(err) {
+			console.log(err);
+		}
+	}
+};
